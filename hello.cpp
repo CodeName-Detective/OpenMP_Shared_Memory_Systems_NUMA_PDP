a7138ec15377fa3e047ed536c26f316436b75b69
Shri <shrithegreat18@gmail.com>
2025-09-21 12:41:20 -0400

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -5,7 +5,7 @@ int main(){
     #pragma omp parallel // Pragma - modifies compiler behaviour tells to use openmp to execute below block in parallel. 
     {
         int ID= omp_get_thread_num();
-        std::cout<<"Hello"<<ID<std::endl<;
+        std::cout<<"Hello"<<ID<<std::endl;
         std::cout<<"World"<<ID<<std::endl;
     } 
     return 0;
